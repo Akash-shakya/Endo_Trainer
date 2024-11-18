2197802a07eb43ebbac809c40f824ae0069b1634
Akash-shakya <akashshakya821@gmail.com>
2024-11-18 14:00:14 +0530

    interface lcd

--- Endo_trainer_with_stepper_motor/Endo_trainer_with_stepper_motor.ino
+++ Endo_trainer_with_stepper_motor/Endo_trainer_with_stepper_motor.ino
@@ -11,9 +11,11 @@ unsigned long debounceDelay = 50;  // 50ms debounce time
 int lastButtonState = LOW;  // previous button state
 int buttonState = LOW;  // current button state
 
-// Define pins for the LEDs (if needed)
-#define ledPin1 12
-#define ledPin2 13
+int count = 1;
+
+// Define pins for the LEDs
+#define redled 12
+#define greenled 13
 
 // Add the lcd
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -23,17 +25,22 @@ void setup() {
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   pinMode(button, INPUT);  // Set the button pin as input
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
+  pinMode(redled, OUTPUT);
+  pinMode(greenled, OUTPUT);
 
   lcd.init();
   lcd.backlight();  // Turn on the LCD backlight
   lcd.print("EndoTrainer Kit");  // Put text on the LCD
+  delay(1000);
+  lcd.clear();
+  lcd.print("Module : 1");
+  lcd.setCursor(3,1);
+  lcd.print("Start Training");
 }
 
 void loop() {
-  // Read the state of the button:
-  int reading = digitalRead(button);
+ 
+  int reading = digitalRead(button);   // Read the state of the button:
 
   // Check if the button state has changed:
   if (reading != lastButtonState) {
@@ -47,33 +54,41 @@ void loop() {
 
       // If the button is pressed, move the motor:
       if (buttonState == HIGH) {
-        // Set the spinning direction clockwise:
-        digitalWrite(dirPin, HIGH);  // Set direction to clockwise
-        digitalWrite(ledPin1, HIGH);  // Turn on LED 1
-        digitalWrite(ledPin2, LOW);   // Turn off LED 2
-        lcd.clear();
-        lcd.print("Waiting...");
-        // Spin the stepper motor 1 revolution slowly:
-        for (int i = 0; i < stepsPerRevolution; i++) {
-          digitalWrite(stepPin, HIGH);
-          delayMicroseconds(8000);  // Adjust for desired motor speed
-          digitalWrite(stepPin, LOW);
-          delayMicroseconds(8000);
-        }
-      } 
-      // If the button is released, reverse direction:
+        count = count +1;
+        if(count<6)
+        {
+          digitalWrite(dirPin, HIGH);  // Set direction to clockwise
+          digitalWrite(redled, HIGH);  // Turn on LED 1
+          digitalWrite(greenled, LOW);   // Turn off LED 2
+          lcd.clear();
+          lcd.print("Module changed");
+          lcd.setCursor(3,1);
+          lcd.print("Waiting....");
+          // Spin the stepper motor 1 revolution slowly:
+          for (int i = 0; i < stepsPerRevolution; i++) {
+            digitalWrite(stepPin, HIGH);
+            delayMicroseconds(8000);  // Adjust for desired motor speed
+            digitalWrite(stepPin, LOW);
+            delayMicroseconds(8000);
+          }
+        } 
+      }
+      if (count ==6)
+      {
+        count = 1;
+      }
       else {
-        digitalWrite(ledPin1, LOW);  // Turn off LED 1
-        digitalWrite(ledPin2, HIGH); // Turn on LED 2
+        digitalWrite(redled, LOW);  // Turn off LED 1
+        digitalWrite(greenled, HIGH); // Turn on LED 2
         digitalWrite(dirPin, LOW);   // Set direction to counterclockwise
         lcd.clear();
-        lcd.print("Module Change");
-        lcd.setCursor(2,1);
-        lcd.print("Start Trainig");
+        lcd.print("Module No :");
+        lcd.setCursor(12,0);
+        lcd.print(count);
+        lcd.setCursor(1,1);
+        lcd.print("Start Training");
       }
     }
   }
-
-  // Save the current button state for next loop iteration
   lastButtonState = reading;
 }
