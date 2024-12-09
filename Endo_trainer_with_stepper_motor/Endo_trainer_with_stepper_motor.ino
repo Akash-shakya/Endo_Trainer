14bc89147d1a152e58b366f670406146fce27ea8
Akash-shakya <akashshakya821@gmail.com>
2024-12-09 16:56:06 +0530

    add new file

--- Endo_trainer_with_stepper_motor/Endo_trainer_with_stepper_motor.ino
+++ Endo_trainer_with_stepper_motor/Endo_trainer_with_stepper_motor.ino
@@ -1,9 +1,11 @@
+#include <LiquidCrystal.h>
+
 // Define pin connections & motor's steps per revolution
 const int dirPin = 2;
 const int stepPin = 3;
 const int buttonPin = 4; // Define the push button pin
 const int anticlockwiseButtonPin = 5;
-const int stepsPerRevolution = 40;
+const int stepsPerRevolution = 320;
 
 // Variable to store the button state
 int buttonState = 0;
@@ -19,7 +21,8 @@ bool motorRunning = false;
 #define greenled 13
 
 int count = 1;
-
+// Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd(6,7,8,9,10,11);
 void setup()
 {
   // Declare pins as Outputs and Inputs
@@ -32,6 +35,12 @@ void setup()
   // Initially, motor is stopped
   digitalWrite(dirPin, LOW);
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.print("Endotrainer kit");
+  delay(2000);
+  lcd.clear();
+
 }
 
 void loop()
@@ -63,8 +72,7 @@ void loop()
   // Update the last button state
   lastButtonState = buttonState;
   lastAnticlockwiseButtonState = anticlockwiseButtonState;
-
-  if(count<6)
+  if(count<6 && count>0)
   {
   // If motorRunning is true, run the motor
       if (motorRunning) {
@@ -72,27 +80,38 @@ void loop()
         
         digitalWrite(redled, HIGH);  
         digitalWrite(greenled, LOW);
-        Serial.println("Module changed");
+        lcd.clear();
+        lcd.print("Module changed");
         // Spin motor slowly for one revolution
         for (int x = 0; x < stepsPerRevolution; x++) {
           digitalWrite(stepPin, HIGH);
-          delayMicroseconds(10000);  // Adjust speed here
+          delayMicroseconds(2000);  // Adjust speed here
           digitalWrite(stepPin, LOW);
-          delayMicroseconds(10000);
+          delayMicroseconds(2000);
         }
     
         // Once motor completes a full revolution, stop
         motorRunning = false;
+        lcd.clear();
       }
       else{
           Serial.println(count);
           Serial.println("Start Training");
           digitalWrite(redled, LOW);  
           digitalWrite(greenled, HIGH);
+          lcd.print("module No : ");
+          lcd.setCursor(13, 0);
+          lcd.print(count);
+          lcd.setCursor(0, 1);
+          lcd.print("Start Training  ");
         }
-        
   }
   else{
-    count = 1;
+    if(count>5)
+    {
+      count = 1;
+    }
+    else
+    count = 5;
     }
 }
